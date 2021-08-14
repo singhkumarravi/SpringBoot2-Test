757f8dbe9b3cd4bb33cb0296d3ba306b999d2c20
Ravi <Ravi@DESKTOP-5A7HVVO>
2021-08-14 17:28:51 +0530

    zxcvx

---
+++ src/main/java/com/olive/array/ArrayToList4.java
@@ -0,0 +1,23 @@
+package com.olive.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayToList4 {
+
+	public static void main(String[] args) {
+		String[] s= {"D","A","S","B"};
+		System.out.println("In array format");
+		for(String s1:s) {
+			System.out.println(s1);
+		}
+		System.out.println("In  array To List ");
+		List<String> list = Arrays.asList(s);
+		           System.out.println(list);
+		Collections.sort(list);
+		System.out.println(list);
+		
+		
+	}
+}
